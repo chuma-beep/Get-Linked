a066b1f4fb36d7d7d95c2b00fde08ebe9b9a9f97
chuma-beep <wisboynelson123@gmail.com>
2024-01-01 20:40:22 +0100

    I nam almost done with the register and contact page

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const icons = document.querySelector('.icon');
 const navMenu = document.querySelector('.nav-list');
 
+// let button = document.querySelector("#button");
+// let closePopup = document.querySelector(".closePopup");
+
+
+
 function mobileNav() {
     navMenu.classList.toggle('active');
     icons.classList.toggle("active");
 }
 
 icons.addEventListener('click', mobileNav);
+
+
+
+
+//    button.addEventListener('click', function () {
+//     myPopup.classList.add("#pop-box")
+//    })
+//    closePopup.addEventListener('click', function (){
+//     myPopup.classList.remove("show")
+//    })
+//    window.addEventListener('click', function (event){
+//     if (event.target == myPopup){
+//     myPopup.classList.remove("show")
+// }
+//    })
 
