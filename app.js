4d9d1e554b558161b307050e97c98acb913a57e7
chuma-beep <wisboynelson123@gmail.com>
2024-01-13 11:45:16 +0100

    added tailwind css

--- app.js
+++ app.js
@@ -64,3 +64,32 @@ window.onload = formcheck
     myPopup.classList.remove("show")
 }
    })
+
+
+
+   //for query selctors for scroll function
+
+
+   //scroll function
+ window.addEventListener('scroll', function() {
+    // keep track of previous scroll position
+let prevScrollPos = window.pageYOffset;
+
+window.addEventListener('scroll', function() {
+  // current scroll position
+  const currentScrollPos = window.pageYOffset;
+
+  if (prevScrollPos > currentScrollPos) {
+    // user has scrolled up
+    document.querySelector('.navbar').classList.add('show');
+  } else {
+    // user has scrolled down
+    document.querySelector('.navbar').classList.remove('show');
+  }
+
+  // update previous scroll position
+  prevScrollPos = currentScrollPos;
+});
+
+  });
+  
